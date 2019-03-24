199eaafcdb6fabb3bf2200ef45aadcc2d7075431
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-24 19:43:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,13 @@
 
 
 def prime?(x)
-  if x 
+  if x < 0 || x == 0 || x == 1 || x == 2
+    false 
+  else 
     divisor = (2..x-1).to_a
   
   divisor.any? do |n|
       x % n == 0
+    end 
   end 
 end 
