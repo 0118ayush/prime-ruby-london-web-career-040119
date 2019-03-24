7487c1185492738fcb95a9f622073c192d075009
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-24 18:24:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 # Add  code here!
 
 
-def prime?(numbers)
-  numbers.each do |x| 
-    if x % x == 0 && x % 1 == 0
+def prime?(number)
+    if  % x == 0 && x % 1 == 0
       true
     else 
       false
